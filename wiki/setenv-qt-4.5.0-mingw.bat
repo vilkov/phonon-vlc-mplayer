f4758bba787667151c167d3fcf7f110b8107102b
tkrotoff <tkrotoff@3e52da23-f849-0410-9240-c733f53fdaa0>
2009-04-17 02:00:43 +0000

    Update setenv scripts

--- wiki/setenv-qt-4.5.0-mingw.bat
+++ wiki/setenv-qt-4.5.0-mingw.bat
@@ -1,9 +1,9 @@
 @echo off
 
-echo Setting up Qt 4.4.3 MinGW...
-set QTDIR=C:\Qt\4.4.3-mingw
+echo Setting up Qt 4.5.0 MinGW...
+set QTDIR=C:\Qt\2009.01\qt
 set PATH=%QTDIR%\bin;%PATH%
-set PATH=%PATH%;C:\MinGW\bin
+set PATH=%QTDIR%\..\bin;%QTDIR%\..\mingw\bin;%PATH%
 set PATH=%PATH%;%SystemRoot%\System32
 set QMAKESPEC=win32-g++
 
